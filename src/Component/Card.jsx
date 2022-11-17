ba7744d9ccaba048e1af92e4af9c64c946c1d270
Freewhisper77 <emmasddx@foxmail.com>
2022-11-17 18:34:00 +0800

    feat: add an input box

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -4,7 +4,6 @@ export default function Card() {
     return (
         <div>
             <h1>My Contacts</h1>
-
             <h2>Beyonce</h2>
             <img
                 src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
